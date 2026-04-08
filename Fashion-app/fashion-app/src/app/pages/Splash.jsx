a997664dc460d605a4bbb9e5d176b7c93106cd57
justmichelle2 <219002197+justmichelle2@users.noreply.github.com>
2026-04-08 20:01:05 +0000

    update

--- Fashion-app/fashion-app/src/app/pages/Splash.jsx
+++ Fashion-app/fashion-app/src/app/pages/Splash.jsx
@@ -8,7 +8,7 @@ export default function Splash() {
   useEffect(() => {
     const timer = setTimeout(() => {
       navigate("/landing");
-    }, 10000);
+    }, 5000);
 
     return () => clearTimeout(timer);
   }, [navigate]);
